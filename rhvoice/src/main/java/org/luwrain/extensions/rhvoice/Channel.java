8d82a8a304c29b2e0661e5c0416c55961d3031e9
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-08-31 22:11:43 +0700

    rhvoice:using speech.ChannelBase

--- rhvoice/src/main/java/org/luwrain/extensions/rhvoice/Channel.java
+++ rhvoice/src/main/java/org/luwrain/extensions/rhvoice/Channel.java
@@ -22,33 +22,26 @@ import java.io.*;
 import java.nio.*;
 import java.nio.file.*;
 import javax.sound.sampled.*;
-import javax.sound.sampled.AudioFormat.Encoding;
 
 import com.github.olga_yakovleva.rhvoice.RHVoiceException;
 import com.github.olga_yakovleva.rhvoice.SynthesisParameters;
-import com.github.olga_yakovleva.rhvoice.TTSClient;
 import com.github.olga_yakovleva.rhvoice.TTSEngine;
 import com.github.olga_yakovleva.rhvoice.VoiceInfo;
 
 import org.luwrain.core.*;
+import org.luwrain.speech.*;
 
-class Channel implements org.luwrain.speech.Channel
+class Channel extends ChannelBase
 {
     static final String LOG_COMPONENT = "rhvoice";
 
-    	static private final double UPPER_CASE_PITCH_MODIFIER = 3.0;
-            static private final double RATE_MIN  = 0.5;
+    static private final double UPPER_CASE_PITCH_MODIFIER = 3.0;
+    static private final double RATE_MIN  = 0.5;
     static private final double RATE_MAX  = 2.0;
     static private final double PITCH_MIN = 0.5;
     static private final double PITCH_MAX = 2.0;
 
-    private int curPitch = 30;
-    private int curRate = 60;
-
-    private String name = "";
-    private boolean defaultChannel = false;
-
-private TTSEngine tts = null;
+    private TTSEngine tts = null;
     private String voiceName = "";
     private SpeakingThread thread = null;
 
@@ -57,9 +50,9 @@ private TTSEngine tts = null;
 	NullCheck.notNull(registry, "registry");
 	NullCheck.notEmpty(path, "path");
 	final Settings sett = Settings.create(registry, path);
-	name = sett.getName("");
+	channelName = sett.getName("");
 	defaultChannel = sett.getDefault(false);
-	if (name.trim().isEmpty())
+	if (channelName.trim().isEmpty())
 	{
 	    Log.error(LOG_COMPONENT, "channel in " + path + " does not have any name");
 	    return false;
@@ -78,10 +71,10 @@ private TTSEngine tts = null;
 	{
 	    Log.warning(LOG_COMPONENT, "unable to load RHVoice_core:" + e.getClass().getName() + ":" + e.getMessage());
 	}
-	    final Path dataPath = Paths.get("rhvoice", "data");
-	    final Path configPath = Paths.get("rhvoice", "config");
-	    final Path enPath = Paths.get("data", "languages", "English");
-	    final Path ruPath = Paths.get("data", "languages", "Russian");
+	final Path dataPath = Paths.get("rhvoice", "data");
+	final Path configPath = Paths.get("rhvoice", "config");
+	final Path enPath = Paths.get("data", "languages", "English");
+	final Path ruPath = Paths.get("data", "languages", "Russian");
 	try {
 	    TTSEngine.init();
 	    tts = new TTSEngine(dataPath.toString(), configPath.toString(), new String[]{
@@ -95,8 +88,8 @@ private TTSEngine tts = null;
 	    return false;
 	}
 	//Selecting the voice
-voiceName = (args.length > 0 && !args[0].isEmpty())?args[0]:suggestVoice();
-if (voiceName == null || voiceName.trim().isEmpty())
+	voiceName = (args.length > 0 && !args[0].isEmpty())?args[0]:suggestVoice();
+	if (voiceName == null || voiceName.trim().isEmpty())
 	{
 	    Log.error(LOG_COMPONENT, "unable to choose a suitable voice");
 	    return false;
@@ -145,29 +138,9 @@ if (voiceName == null || voiceName.trim().isEmpty())
     	return voices;
     }
 
-    @Override public String getChannelName()
-    {
-	return name;
-    }
-
     @Override public Set<Features> getFeatures()
     {
 	return EnumSet.of(Features.CAN_SYNTH_TO_SPEAKERS,Features.CAN_NOTIFY_WHEN_FINISHED); // 
-    }
-
-    @Override public boolean isDefault()
-    {
-	return defaultChannel;
-    }
-
-    @Override public void setDefaultPitch(int value)
-    {
-    	curPitch=properRange(value);
-    }
-
-    @Override public void setDefaultRate(int value)
-    {
-    	curRate=properRange(value);
     }
 
     @Override public long speak(String text,Listener listener,int relPitch,int relRate, boolean cancelPrevious)
@@ -175,8 +148,8 @@ if (voiceName == null || voiceName.trim().isEmpty())
 	NullCheck.notNull(text, "text");
 	final SynthesisParameters p = new SynthesisParameters();
 	p.setVoiceProfile(voiceName);
-	p.setRate(convRate(curRate + relRate));
-	p.setPitch(convPitch(curPitch + relPitch));
+	p.setRate(convRate(defaultRate + relRate));
+	p.setPitch(convPitch(defaultPitch + relPitch));
    	p.setSSMLMode(false);
 	runThread(text,listener, p);
 	return -1;
@@ -186,22 +159,22 @@ if (voiceName == null || voiceName.trim().isEmpty())
     {
 	final SynthesisParameters p = new SynthesisParameters();
 	p.setVoiceProfile(voiceName);
-	p.setRate(convRate(curRate + relRate));
-	p.setPitch(convPitch(curPitch + relPitch) + (Character.isUpperCase(letter)?UPPER_CASE_PITCH_MODIFIER:0));
+	p.setRate(convRate(defaultRate + relRate));
+	p.setPitch(convPitch(defaultPitch + relPitch) + (Character.isUpperCase(letter)?UPPER_CASE_PITCH_MODIFIER:0));
    	p.setSSMLMode(false);
 	runThread("" + letter, listener, p);
    	return -1;
     }
 
     private void runThread(String text, Listener listener, SynthesisParameters params)
-{
-    NullCheck.notNull(text, "text");
-    NullCheck.notNull(params, "params");
-    if (thread != null)
-	thread.interrupt();
-    thread = new SpeakingThread(text, listener, this, params);
-    new Thread(thread).start();
-	}
+    {
+	NullCheck.notNull(text, "text");
+	NullCheck.notNull(params, "params");
+	if (thread != null)
+	    thread.interrupt();
+	thread = new SpeakingThread(text, listener, this, params);
+	new Thread(thread).start();
+    }
 
     @Override public boolean synth(String text,int pitch, int rate, AudioFormat format,OutputStream stream)
     {
@@ -237,30 +210,11 @@ if (voiceName == null || voiceName.trim().isEmpty())
 	if (thread != null)
 	    thread.interrupt();
 	thread = null;
-	}
+    }
 
     @Override public AudioFormat[] getSynthSupportedFormats()
     {
 	return null;
-    }
-
-    @Override public void setCurrentPuncMode(PuncMode mode)
-    {
-    }
-
-    @Override public PuncMode getCurrentPuncMode()
-    {
-	return null;
-    }
-
-    @Override public int getDefaultRate()
-    {
-	return 50;
-    }
-
-    @Override public int getDefaultPitch()
-    {
-	return 100;
     }
 
     @Override public void setCurrentVoice(String name)
@@ -275,21 +229,13 @@ if (voiceName == null || voiceName.trim().isEmpty())
     @Override public void close()
     {
 	silence();
-	//FIXME:
+	//FIXME:is anything else needed there?
+	tts = null;
     }
 
     TTSEngine getTtsEngine()
     {
 	return tts;
-    }
-
-    static private int properRange(int value)
-    {
-	if(value < 0)
-	    return 0;
-	if(value > 100)
-	    return 100;
-	return value;
     }
 
     static private double convRate(int value)
