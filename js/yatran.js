5f191bf1a00e486314564af1437d2e97809507d1
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-06-22 05:32:14 +0700

    yatran.js works well

--- js/yatran.js
+++ js/yatran.js
@@ -1,28 +1,32 @@
 
 var KEY = "trnsl.1.1.20150921T153654Z.976f9d18953f9209.b06292c929d38cffaa9d8e5f724898fa4c9c382d";
 
-var yatran = function()
+function yatran(text, lang)
 {
-    var text = "";
     var url = "https://translate.yandex.net/api/v1.5/tr/translate?key=" + KEY + "&text=";
     url += java.net.URLEncoder.encode(text);
-    url += "&lang=en-ru";
-    
-    Luwrain.runBkg(function(){
-
-	var res = "";
-
-	var con = org.jsoup.Jsoup.connect(url);
-var doc = con.get();
-var items = doc.getElementsByTag("text");
-for(var i = 0;i < items.length;i++)
-	{
-	    res += items[i].text();
-    }
-
-	
-	Luwrain.message(res);
-    });
+    url += "&lang=";
+    url += lang;
+    var res = "";
+    var con = org.jsoup.Jsoup.connect(url);
+    var doc = con.get();
+    var items = doc.getElementsByTag("text");
+    for(var i = 0;i < items.length;i++)
+	res += items[i].text();
+    Luwrain.message(res);
 }
 
-Luwrain.addCommand("yatran2", yatran);
+function yatranEnRu()
+{
+    var text = Luwrain.getActiveAreaText("region");
+    if (text == null)
+	text = Luwrain.getActiveAreaText("word");
+    if (text == null || text.trim().isEmpty())
+    {
+	Luwrain.message("Отсутствует текст для перевода");//FIXME:error type
+	return;
+    }
+        Luwrain.runBkg(function(){yatran(text, "en-ru");});
+}
+
+Luwrain.addCommand("yatran-en-ru", yatranEnRu);
