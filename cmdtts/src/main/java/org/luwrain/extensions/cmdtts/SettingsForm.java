7846a119a841b98ad92e90692e5785be53f35166
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-06-23 03:30:43 +0700

    Renaming Area.onEnvironmentEvent() to onSystemEvent()

--- cmdtts/src/main/java/org/luwrain/extensions/cmdtts/SettingsForm.java
+++ cmdtts/src/main/java/org/luwrain/extensions/cmdtts/SettingsForm.java
@@ -51,12 +51,12 @@ class SettingsForm extends FormArea implements SectionArea
 	return super.onKeyboardEvent(event);
     }
 
-    @Override public boolean onEnvironmentEvent(EnvironmentEvent event)
+    @Override public boolean onSystemEvent(EnvironmentEvent event)
     {
 	NullCheck.notNull(event, "event");
-	if (controlPanel.onEnvironmentEvent(event))
+	if (controlPanel.onSystemEvent(event))
 	    return true;
-	return super.onEnvironmentEvent(event);
+	return super.onSystemEvent(event);
     }
 
     @Override public boolean saveSectionData()
