eaec7cbbf98e9d58a1f0b8b555e2bffb77947df0
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-03-14 22:58:30 +0700

    web.js: Intermediate commit

--- js/x-web.js
+++ js/x-web.js
@@ -1,5 +1,6 @@
 
 var USER_AGENT = "MOZILLA/5.0";
+
 function makeGoogleSearchUrl(query, lang)
 {
     return "http://www.google.ru/search?q=" + java.net.URLEncoder.encode(query) + "&hl=" + lang + "&ie=utf-8";
@@ -7,9 +8,9 @@ function makeGoogleSearchUrl(query, lang)
 
 function getAttr(node)
 {
-    	    var attr = node.attributes();
-	    if (attr != null)
-		attr = Java.from(attr.asList());
+    var attr = node.attributes();
+    if (attr != null)
+	attr = Java.from(attr.asList());
     if (attr == null)
 	return {array: [], map: {}};
     var res = {array: [], map: {}, id: "", cl: ""};
@@ -31,6 +32,79 @@ function getAttr(node)
     return res;
 }
 
+function getNodeText(node)
+{
+    var children = node.childNodes();
+    if (children == null)
+	return "";
+        var res = "";
+    for(var i = 0;i < children.length;i++)
+    {
+	var cl = children[i].getClass().getName();
+	if (cl == "org.jsoup.nodes.Element")
+	{
+	    res += getNodeText(children[i]);
+	    continue;
+	}
+	if (cl == "org.jsoup.nodes.TextNode")
+	{
+res += children[i].text();
+	    continue;
+    }
+    }
+return res;
+}
+
+
+function googleFindRes(node, result)
+{
+    var children = node.childNodes();
+    if (children == null)
+	return;
+    for(var i = 0;i < children.length;i++)
+    {
+	var cl = children[i].getClass().getName();
+	if (cl == "org.jsoup.nodes.Element")
+	{
+	    var tagName = children[i].nodeName();
+	    var attr = getAttr(children[i]);
+	    var id = attr.id.toLowerCase();
+	    if (id == "res")
+	    {
+		result.items = children[i];
+		return;
+	    }
+	    googleFindRes(children[i], result);
+	    if (result.items != null)
+		return;
+	    continue;
+	}
+    }
+}
+
+function googleParseItem(node, result)
+{
+    var header = node.getElementsByTag("h3");
+    if (header != null && header.length == 1)
+	header = getNodeText(header[0]);
+    print("Header: " + header);
+    //printNodes(node);
+    var cite = node.getElementsByTag("cite");
+    if (cite != null && cite.length == 1)
+	cite = getNodeText(cite[0]);
+    print("Cite: " + cite);
+    /*
+    var stored = node.getElementsByTag("li");
+    print(stored[0]);
+    /*
+    if (stored != null && stored.length == 1)
+	stored = stored[0];
+    printNodes(stored);
+    */
+    */
+}
+
+
 function printNodes(node)
 {
     var children = node.childNodes();
@@ -43,12 +117,10 @@ function printNodes(node)
 	{
 	    var tagName = children[i].nodeName();
 	    var attr = getAttr(children[i]);
-	    if (tagName.toLowerCase() == "script")
-		continue;
+	    var id = attr.id.toLowerCase();
 	    print("<" + tagName + ">");
 	    if (!attr.id.isEmpty())
 		print("ID: " + attr.id);
-
 	    printNodes(children[i]);
 	    continue;
 	}
@@ -57,18 +129,22 @@ function printNodes(node)
 	    print("\"" + children[i].text() + "\"");
 	    continue;
 	}
-	print(children[i].getClass().getName());
     }
 }
-
 
 Luwrain.addCommand("web-search-google", function(){
     var url = makeGoogleSearchUrl("super", "ru");
     var con = org.jsoup.Jsoup.connect(url);
     con.userAgent(USER_AGENT);
     var doc = con.get();
+    var result = {items: null};
+    googleFindRes(doc.body(), result);
+    if (result.items != null)
+    {
+	var items = result.items.children()[1].children()[0].children()[0].children();
+	for(var i = 0;i < items.length;i++)
+	googleParseItem(items[i]);
+    }
 
-    printNodes(doc.body());
-	
     Luwrain.message("ok");
 });
