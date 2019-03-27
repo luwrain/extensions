b42c4e22d18ffdac5a123248dd5708ca224c1dc5
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-03-27 22:26:37 +0700

    Adding reader.js

---
+++ js/reader.js
@@ -0,0 +1,54 @@
+/*
+   Copyright 2019 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+function onIoException(ex, lines)
+{
+    lines.push("Ошибка ввода/вывода:");
+    if (ex.getClass().getName().equals("java.io.FileNotFoundException"))
+	lines.push("Файл не найден"); else
+	    lines.push(ex.getClass().getName());
+    var msg = ex.getMessage();
+    if (msg != null && !msg.trim().isEmpty())
+	lines.push(msg.trim());
+}
+
+function onException (ex, lines)
+{
+    var cause = ex.getCause();
+    if (ex.getClass().getName().equals("java.lang.RuntimeException") && cause != null)
+    {
+	if (java.io.IOException.class.isInstance(cause))
+	{
+	    onIoException(cause, lines);
+	    return;
+	}
+	onException(cause);
+	return;
+    }
+    lines.push(ex.getClass().getName());
+    var msg = ex.getMessage();
+    if (msg != null && !msg.trim().isEmpty())
+	lines.push(msg);
+}
+
+Luwrain.addHook("luwrain.reader.doc.error", function(props, ex){
+    var res = [];
+    res.push("Невозможно открыть документ");
+    res.push("");
+    if (ex != null)
+	onException(ex, res);
+    return res;
+    });
