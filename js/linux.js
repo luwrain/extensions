8d9e0367d8ffbace31617fc0ab9f3ce155a9729d
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-04-04 06:36:01 +0700

    Adding linux.js

---
+++ js/linux.js
@@ -0,0 +1,41 @@
+/*
+   Copyright 2019 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+function getTypeDescr(type)
+{
+    switch(type)
+    {
+    case "volume_root":
+	return Luwrain.i18n.static.partitionsPopupItemRoot;
+    case "volume_user_home":
+	return Luwrain.i18n.static.partitionsPopupItemUserHome;
+    case "volume_regular":
+	return Luwrain.i18n.static.partitionsPopupItemRegular;
+    case "volume_remote":
+	return Luwrain.i18n.static.partitionsPopupItemRemote;
+    case "volume_removable":
+	return Luwrain.i18n.static.partitionsPopupItemRemovable;
+    default:
+	return "";
+    }
+}
+
+Luwrain.addHook("luwrain.linux.popups.disks.list", function(volumes){
+    var res = [];
+    for(var i = 0;i < volumes.length;i++)
+	res.push({title: volumes[i].name + " " + getTypeDescr(volumes[i].type)});
+    return res;
+});
