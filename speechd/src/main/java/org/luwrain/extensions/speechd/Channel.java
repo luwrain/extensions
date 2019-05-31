de8e788e400226338c4b5da20f8a442cc9f3039b
Michael Pozhidaev <msp@luwrain.org>
2019-05-31 11:03:40 +0700

    speechd: Setting pitch, rate and the language

--- speechd/src/main/java/org/luwrain/extensions/speechd/Channel.java
+++ speechd/src/main/java/org/luwrain/extensions/speechd/Channel.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2012-2019 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 package org.luwrain.extensions.speechd;
 
@@ -21,6 +36,8 @@ final class Channel implements org.luwrain.speech.Channel
     {
 	NullCheck.notNull(params, "params");
 	client = new SSIPClient("LUWRAIN", null, null);
+	if (client != null && params.containsKey("lang"))
+	    client.setLanguage(params.get("lang"));
     }
 
     @Override public String getChannelName()
@@ -46,6 +63,8 @@ final class Channel implements org.luwrain.speech.Channel
 				int relPitch, int relRate, boolean cancelPrevious)
     {
 	try {
+	    client.setPitch(relPitch * 2);
+	    client.setRate(-1 * relRate * 2);
 	    client.say(SSIPPriority.TEXT, text);
 	}
 	catch(Exception e)
@@ -59,11 +78,13 @@ final class Channel implements org.luwrain.speech.Channel
 				      int relPitch, int relRate, boolean cancelPrevious)
     {
 	try {
-	client.say(SSIPPriority.TEXT, "" + letter);
+	    client.setPitch(relPitch * 2);
+	    client.setRate(-1 * relRate * 2);
+	    client.sayChar(SSIPPriority.TEXT, letter);
 	}
 	catch(Exception e)
 	{
-	    	    Log.error(LOG_COMPONENT, "unable to speak a text:" + e.getClass().getName() + ":" + e.getMessage());
+	    Log.error(LOG_COMPONENT, "unable to speak a text:" + e.getClass().getName() + ":" + e.getMessage());
 	}
 	return -1;
     }
@@ -75,9 +96,23 @@ final class Channel implements org.luwrain.speech.Channel
 
     @Override public void silence()
     {
+	try {
+	    client.stop();
+	}
+	catch(Exception e)
+	{
+	    Log.error(LOG_COMPONENT, "unable to stop the speech:" + e.getClass().getName() + ":" + e.getMessage());
+	}
     }
 
     @Override public void close()
     {
+	try {
+	    client.close();
+	}
+	catch(Exception e)
+	{
+	    Log.error(LOG_COMPONENT, "unable to close the connection:" + e.getClass().getName() + ":" + e.getMessage());
+	}
     }
 }
--- speechd/src/main/java/org/luwrain/extensions/speechd/Extension.java
+++ speechd/src/main/java/org/luwrain/extensions/speechd/Extension.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2012-2019 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 package org.luwrain.extensions.speechd;
 
@@ -29,7 +44,7 @@ public final class Extension extends EmptyExtension
 		    }
 		    catch(Exception e)
 		    {
-			Log.error(LOG_COMPONENT, "Unable to create the command line speech channel:" + e.getClass().getName() + ":" + e.getMessage());
+			Log.error(LOG_COMPONENT, "Unable to create the speech-dispatcher channel:" + e.getClass().getName() + ":" + e.getMessage());
 			return null;
 		    }
 		}
