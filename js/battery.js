6ce01d570038c10f980f47be2f339f4199b74da9
Michael Pozhidaev <msp@luwrain.org>
2019-06-24 23:02:57 +0800

    Adding initial math-latex.js

--- js/battery.js
+++ js/battery.js
@@ -1,5 +1,6 @@
 
 var pattern = java.util.regex.Pattern.compile("Battery ([0-9]+): +([a-zA-Z]+), +([0-9]+)%, +([0-9:]+) .*");
+var pattern2 = java.util.regex.Pattern.compile("Battery ([0-9]+): +([a-zA-Z]+), +([0-9]+)%.*");
 
 function getBatteries()
 {
@@ -19,6 +20,18 @@ function getBatteries()
 		level: matcher.group(3),
 		estimation: matcher.group(4)};
 	    res.push(battery);
+	} else
+	{
+	matcher = pattern2.matcher(line);
+	if (matcher.find())
+	{
+	    var battery = {
+		id: matcher.group(1),
+		state: matcher.group(2).toLowerCase(),
+		level: matcher.group(3),
+		estimation: ""};
+	    res.push(battery);
+	}
 	}
 	line = r.readLine();
     }
---
+++ js/math-latex.js
@@ -0,0 +1,62 @@
+#!/usr/bin/jjs
+
+function commit(item, res)
+{
+    if (current.type === 'none')
+	return;
+    res.push(current);
+}
+
+var res = [];
+var line = "\\sqrt x \\frac {y}{z}";
+
+var current = {type: 'none'};
+
+for(var i = 0;i < line.length;i++)
+{
+    var c = line[i];
+    //backslash
+    if (c === '\\')
+    {
+	commit(current, res);
+	current = {type: 'none'};
+	res.push({type: 'backslash'});
+	continue;
+    }
+    //block
+    if (c === '{' || c === '(' || c === '}' || c === ')')
+    {
+	commit(current, res);
+	current = {type: 'none'};
+	res.push({type: 'block', value: c});
+	continue;
+    }
+    //space
+    if (line[i] === ' ')
+    {
+	commit(current, res);
+	current = {type: 'none'};
+	res.push({type: 'space'});
+	continue;
+    }
+    //letters
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+	if (current.type != 'letters')
+	{
+	    commit(current, res);
+	    current.type = 'letters';
+	    current.value = '';
+	}
+	current.value += c;
+	continue;
+    }
+}
+commit(current, res);
+
+print(res.length);
+
+for(var i = 0;i < res.length;i++)
+{
+    print(res[i].type + " " + res[i].value);
+}
