be14f1a85feaf5f5b63c027b4962ae47e747fa87
Michael Pozhidaev <msp@luwrain.org>
2019-07-22 04:19:31 +0700

    dd.js: Adding extractSearchItemData()

--- js/web-ddg.js
+++ js/web-ddg.js
@@ -1,35 +1,18 @@
 
 
-function ddgQuery(query)
+function extractSearchItemData(el)
 {
     
-var url = 'http://duckduckgo.com/html/?q=' + java.net.URLEncoder.encode(query);
-
-
-
-var con = org.jsoup.Jsoup.connect(url);
-var doc = con.get();
-var items = doc.getElementsByTag("div");
-var res = []; 
-for(var i = 0;i < items.length;i++)
-{
-    var text = items[i].toString();
-    if (text.indexOf("form") >= 0)
-	continue;
-    if (text.indexOf("result__title") < 0)
-	continue;
-    if (text.indexOf("results_links_deep") >= 0)
-	continue;
-
     var title = '';
-    var h2 = items[i].getElementsByTag("h2");
-    for(var j = 0;j < h2.length;j++)
-	title = h2[j].text();
-
-    var snippet = '';
+        var snippet = '';
     var displayUrl = '';
     var clickUrl = '';
-    var children = Java.from(items[i].childNodes());
+
+    
+    var h2 = el.getElementsByTag("h2");
+    for(var j = 0;j < h2.length;j++)
+	title = h2[j].text();
+    var children = Java.from(el.childNodes());
     for(var j = 0;j < children.length;j++)
     {
 	if (children[j].getClass().getSimpleName() != "Element")
@@ -44,8 +27,26 @@ clickUrl = a[k].attr("href");
 	if (children[j].attr("class") === 'result__snippet')
 snippet = children[j].text();
     }
+return     {title: title, snippet: snippet, displayUrl: displayUrl, clickUrl: clickUrl};
+}
 
-    res.push({title: title, snippet: snippet, displayUrl: displayUrl, clickUrl: clickUrl});
+function ddgQuery(query)
+{
+var url = 'http://duckduckgo.com/html/?q=' + java.net.URLEncoder.encode(query);
+var con = org.jsoup.Jsoup.connect(url);
+var doc = con.get();
+var items = doc.getElementsByTag("div");
+var res = []; 
+for(var i = 0;i < items.length;i++)
+{
+    var text = items[i].toString();
+    if (text.indexOf("form") >= 0)
+	continue;
+    if (text.indexOf("result__title") < 0)
+	continue;
+    if (text.indexOf("results_links_deep") >= 0)
+	continue;
+    res.push(extractSearchItemData(item[i]));
 }
     return res;
 }
