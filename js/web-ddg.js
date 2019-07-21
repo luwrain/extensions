4b703862ed19b023a1998ae84a540c6a02b4533e
Michael Pozhidaev <msp@luwrain.org>
2019-07-22 04:22:03 +0700

    ddg.js: Indentation fixes

--- js/web-ddg.js
+++ js/web-ddg.js
@@ -1,14 +1,25 @@
+/*
+   Copyright 2019 Michael Pozhidaev <msp@luwrain.org>
 
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 function extractSearchItemData(el)
 {
-    
     var title = '';
-        var snippet = '';
+    var snippet = '';
     var displayUrl = '';
     var clickUrl = '';
-
-    
     var h2 = el.getElementsByTag("h2");
     for(var j = 0;j < h2.length;j++)
 	title = h2[j].text();
@@ -19,34 +30,34 @@ function extractSearchItemData(el)
 	    continue;
 	if (children[j].attr("class") === 'result__extras')
 	{
-displayUrl = children[j].text();
+	    displayUrl = children[j].text();
 	    var a = children[j].getElementsByTag("a");
 	    for(var k = 0;k < a.length;k++)
-clickUrl = a[k].attr("href");
-		     }
+		clickUrl = a[k].attr("href");
+	}
 	if (children[j].attr("class") === 'result__snippet')
-snippet = children[j].text();
+	    snippet = children[j].text();
     }
-return     {title: title, snippet: snippet, displayUrl: displayUrl, clickUrl: clickUrl};
+    return     {title: title, snippet: snippet, displayUrl: displayUrl, clickUrl: clickUrl};
 }
 
 function ddgQuery(query)
 {
-var url = 'http://duckduckgo.com/html/?q=' + java.net.URLEncoder.encode(query);
-var con = org.jsoup.Jsoup.connect(url);
-var doc = con.get();
-var items = doc.getElementsByTag("div");
-var res = []; 
-for(var i = 0;i < items.length;i++)
-{
-    var text = items[i].toString();
-    if (text.indexOf("form") >= 0)
-	continue;
-    if (text.indexOf("result__title") < 0)
-	continue;
-    if (text.indexOf("results_links_deep") >= 0)
-	continue;
-    res.push(extractSearchItemData(item[i]));
-}
+    var url = 'http://duckduckgo.com/html/?q=' + java.net.URLEncoder.encode(query);
+    var con = org.jsoup.Jsoup.connect(url);
+    var doc = con.get();
+    var items = doc.getElementsByTag("div");
+    var res = []; 
+    for(var i = 0;i < items.length;i++)
+    {
+	var text = items[i].toString();
+	if (text.indexOf("form") >= 0)
+	    continue;
+	if (text.indexOf("result__title") < 0)
+	    continue;
+	if (text.indexOf("results_links_deep") >= 0)
+	    continue;
+	res.push(extractSearchItemData(item[i]));
+    }
     return res;
 }
