71599cc954da93f783d03e30af7aab1e855d9c89
Michael Pozhidaev <msp@luwrain.org>
2019-07-23 00:43:54 +0700

    Adding studio-tex.js

---
+++ js/studio-tex.js
@@ -0,0 +1,12 @@
+
+
+
+Luwrain.addHook("luwrain.studio.project.types", function(){
+    return [
+	{id: "tex-presentation", orderIndex: 0, title: "Презентация TeX"},
+	{id: "tex-article", orderIndex: 1, title: "Статья TeX"},
+	{id: "tex-thesis", orderIndex: 2, title: "Диссертация TeX"},
+	{id: "tex-thesis-referat", orderIndex: 3, title: "Автореферат диссертации TeX"},
+		{id: "tex-book", orderIndex: 4, title: "Книга TeX"},
+    ];
+});
