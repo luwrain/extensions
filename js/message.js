d6d6575f811073a66b27454813e3dd3439892ffd
Michael Pozhidaev <msp@luwrain.org>
2019-08-15 15:20:09 +0700

    message.js: Lines splitting works properly

--- js/message.js
+++ js/message.js
@@ -14,7 +14,7 @@
    General Public License for more details.
 */
 
-var MAX_LINE_LEN = 10;
+var MAX_LINE_LEN = 60;
 
 Luwrain.addHook("luwrain.message.edit.insert.chars.post", function(args){
     if (args.chars != ' ')
@@ -24,19 +24,37 @@ Luwrain.addHook("luwrain.message.edit.insert.chars.post", function(args){
     var lines = args.lines;
     var index = args.hotPoint.y;
     var line = lines[index];
-    if (args.x >= line.length || args.hotPoint.x >= line.lenght)
+    if (args.x >= line.length || args.hotPoint.x > line.length)
 	return;
     if (line.length <= MAX_LINE_LEN || args.x <= MAX_LINE_LEN)
 	return;
     var pos = -1;
-    for(var i = 0;i < args.x;i++)
+    //Looking for the latest space prior to MAX_LINE_LEN
+    for(var i = 0;i < MAX_LINE_LEN;i++)
 	if (line[i] == ' ')
+	    pos = i;
+    //Or the first space after MAX_LINE_LEN
+    if (pos < 0)
+	for(var i = MAX_LINE_LEN;i < args.x;i++)
+	    if (line[i] == ' ')
+    {
 	pos = i;
+	break;
+    }
         if (pos < 0)
-	return;
-
-    lines[index] = line.substring(0, pos);
-    lines.insert(index + 1, line.substring(pos));
-    args.hotPoint.x -= pos;
-    args.hotPOint.y++;
+	    return;
+    //Cutting the current line on pos with  removing trailing spaces
+    var line1 = line.substring(0, pos);
+    var cutPos = pos - 1;
+    while (cutPos >= 0 && line1[cutPos] == ' ')
+	cutPos--;
+	line1 = line1.substring(0, cutPos + 1);
+    //Skipping all spaces on split point at the beginning of the next line
+    cutPos = pos;
+    while(cutPos < args.x && line[cutPos] == ' ')
+	cutPos++;
+    lines.insert(index + 1, line.substring(cutPos));
+    args.hotPoint.x -= cutPos;
+    args.hotPoint.y++;
+    lines[index] = line1;
 });
