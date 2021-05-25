cdd08ec09eb000d82545f7a2845745b86dbde534
Michael Pozhidaev <msp@luwrain.org>
2021-05-25 12:33:48 +0700

    Removing needless player functions in JS

--- js/player.js
+++ js/player.js
@@ -1,5 +1,5 @@
 /*
-   Copyright 2019-2020 Michael Pozhidaev <msp@luwrain.org>
+   Copyright 2019-2021 Michael Pozhidaev <msp@luwrain.org>
 
    This file is part of LUWRAIN.
 
@@ -14,14 +14,6 @@
    General Public License for more details.
 */
 
-Luwrain.createPropertyHook("luwrain.player.track.sec", "luwrain.prop.player.track.sec");
-Luwrain.addHook("luwrain.prop.player.track.sec", function(propName, propValue){
-});
-
-Luwrain.createPropertyHook("luwrain.player.track.url", "luwrain.prop.player.track.url");
-Luwrain.addHook("luwrain.prop.player.track.url", function(propName, propValue){
-});
-
 Luwrain.createPropertyHook("luwrain.player.track.index", "luwrain.prop.player.track.index");
 Luwrain.addHook("luwrain.prop.player.track.index", function(propName, propValue){
     Luwrain.sounds.playing();
@@ -35,177 +27,6 @@ function isStreaming()
 	    return true;
     return false;
 }
-
-function getFilesInDir(dir)
-{
-    var res = [];
-    var items = dir.listFiles();
-    if (items == null || items.length == 0)
-	return [];
-    java.util.Arrays.sort(items);
-    for(var i = 0;i < items.length;i++)
-	if (items[i].isDirectory())
-	{
-	    var subdirItems = getFilesInDir(items[i]);
-	    for(var j = 0;j < subdirItems.length;j++)
-		res.push(subdirItems[j]);
-	}
-    for(var i = 0;i < items.length;i++)
-	if (items[i].isFile())
-	    res.push(items[i]);
-    return res;
-}
-
-
-Luwrain.addHook("luwrain.player.album.play", function(album){
-    switch(album.type)
-    {
-    case "dir":
-	{
-	    var path = "" + album.properties.path;
-	    if (path.isEmpty())
-		return false;
-	    var files = getFilesInDir(new java.io.File(path));
-	    if (files.length == 0)
-		return false;
-	    var urls = [];
-	    for(var i = 0;i < files.length;i++)
-		urls.push(org.luwrain.util.UrlUtils.fileToUrl(files[i]).toString());
-	    Luwrain.player.play(urls, 0, 0, [], album.properties);
-	    return true;
-	}
-    case "streaming":
-	{
-	    var url = "" + album.properties.url;
-	    if (url.isEmpty())
-		return false;
-	    Luwrain.sounds.playing();
-	    Luwrain.player.play([album.properties.url], 0, 0, ["streaming"], album.properties);
-	    return true;
-	}
-    default:
-	return false;
-    }
-});
-
-function commonKeys(event)
-{
-    if (Luwrain.player.state === "stopped")
-    {
-	if(event.special != null)
-	    return false;
-	switch(event.ch)
-	{
-		case "_":
-	    {
-		if (!event.withShiftOnly)
-		return false;
-		    var level = Luwrain.player.getVolume();
-	    if (level > 5)
-		level -= 5; else
-		    level = 0;
-	    Luwrain.message("Громкость " + level);
-	    Luwrain.player.setVolume(level);
-	    return true;
-	}
-	case "+":
-	{
-	    	    if (!event.withShiftOnly)
-		return false;
-		    var level = Luwrain.player.getVolume();
-	    if (level < 95)
-		level += 5; else
-		    level = 100;
-	    Luwrain.message("Громкость " + level);
-		    Luwrain.player.setVolume(level);
-	    return true;
-	}
-	default:
-	return false;
-    }
-    }
-    //The player has a playlist
-    if (event.special != null)
-        switch(event.special)
-        {
-	    case "escape":
-	    Luwrain.sounds.playing();
-	    Luwrain.player.stop();
-	    return true;
-	    default:
-	    return false;
-	}
-    switch(event.ch)
-    {
-	case " ":
-	if (isStreaming())
-	    Luwrain.sounds.playing();
-	Luwrain.player.pauseResume();
-	return true;
-	case "-":
-	if (isStreaming())
-	    return false;
-	if (!event.modified)
-	    Luwrain.player.jump(-5000); else
-		if (event.withControlOnly)
-		    	    Luwrain.player.jump(-60000); else
-		if (event.withAltOnly)
-	{
-	    if (Luwrain.player.prevTrack())
-		Luwrain.sounds.playing(); else
-		    return false;
-	} else
-		return false;
-	return true;
-		case "=":
-	if (isStreaming())
-	    return false;
-	if (!event.modified)
-	    Luwrain.player.jump(5000); else
-		if (event.withControlOnly)
-		    Luwrain.player.jump(60000); else
-			if (event.withAltOnly)
-	{
-	    if (Luwrain.player.nextTrack())
-		Luwrain.sounds.playing(); else
-		    return false;
-	} else
-		return false;
-	return true;
-		case "_":
-	{
-	    if (!event.withShiftOnly)
-		return false;
-		    var level = Luwrain.player.getVolume();
-	    if (level > 5)
-		Luwrain.player.setVolume(level - 5); else
-		    Luwrain.player.setVolume(0);
-	    return true;
-	}
-	case "+":
-	{
-	    	    if (!event.withShiftOnly)
-		return false;
-		    var level = Luwrain.player.getVolume();
-	    if (level < 95)
-		Luwrain.player.setVolume(level + 5); else
-		    Luwrain.player.setVolume(100);
-	    return true;
-	}
-	default:
-	return false;
-    }
-}
-
-Luwrain.addHook("luwrain.app.player.areas.albums.input", function(event, album){
-    return commonKeys(event);
-});
-Luwrain.addHook("luwrain.app.player.areas.playlist.input", function(event, track){
-    return commonKeys(event);
-});
-Luwrain.addHook("luwrain.app.player.areas.control.input", function(event){
-    return commonKeys(event);
-});
 
 Luwrain.addCommand("player-pause", function(){
     if (Luwrain.player.state === "stopped")
