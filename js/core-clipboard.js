c0ba5ce5ae3f04302fd00ae64299b0b033025145
Michael Pozhidaev <msp@luwrain.org>
2022-06-03 16:36:01 +0700

    Minor fixes to JS

--- js/core-clipboard.js
+++ js/core-clipboard.js
@@ -3,3 +3,8 @@ Luwrain.addHook("luwrain.area.region.point.set", function(arg){
     Luwrain.speak("Отметка установлена", Luwrain.const.SOUND_REGION_POINT);//FIXME:
     return true;
 });
+
+Luwrain.addHook("luwrain.clipboard.copy.all", function(arg){
+    Luwrain.speak("Скопировано полное содержимое в буфер обмена", Luwrain.const.SOUND_COPIED);//FIXME:
+    return true;
+});
--- js/nashorn-clipboard.js
+++ js/nashorn-clipboard.js
@@ -1,10 +1,4 @@
 
-Luwrain.addHook("luwrain.clipboard.copy.all", function(arg){
-    Luwrain.sounds.copied();
-    Luwrain.speak("Скопировано полное содержимое в буфер обмена");//FIXME:
-    return true;
-});
-
 Luwrain.addHook("luwrain.area.clear", function(arg){
     Luwrain.sounds.deleted();
     Luwrain.speak("Очищено");//FIXME:
